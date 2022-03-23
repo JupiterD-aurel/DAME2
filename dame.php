0f9ef761e76183a23fa71cbf87e5972c26bcac82
JupiterD-aurel <92156529+JupiterD-aurel@users.noreply.github.com>
2022-03-23 11:24:35 +0100

    Add files via upload

---
+++ dame.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Dame</title>
+</head>
+<body>
+
+
+    <div class="block">
+
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+?>
+    <?php
+$a =
+[
+    "blanc",
+    "noir",
+   
+];
+
+$i = 0;
+
+while ($i <= 8)
+{
+    echo $a[$i % 2];
+    ++$i;
+}
+echo "</br>"
+?>
+
+    </div>
+
+</body>
+</html>
